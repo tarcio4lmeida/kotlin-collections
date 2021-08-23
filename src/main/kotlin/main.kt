c200b46fccb56d6190e96a88fe23d734a6b5a3e7
tarcio almeida <tarcioalmeidalima@gmail.com>
2021-08-23 11:00:55 -0300

    tipos de colecao

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,39 @@
-fun main(){
-    val nomes: Collection<String> = listOf("Alex", "Fran", "Gui", "Maria", "Ana")
+fun main() {
+
+    val banco = BancoDeNomes()
+    banco.salva("Alex")
+    println(banco.nomes)
+
+}
+
+class BancoDeNomes{
+
+
+    val nomes : Collection<String>  get() = dados
+
+    fun salva(nome: String){
+        dados.add(nome)
+    }
+
+    companion object{
+        private val dados = mutableListOf<String>()
+    }
+}
+fun testaColecao() {
+    val nomes: Collection<String> = mutableListOf(
+        "Alex",
+        "Fran",
+        "Gui",
+        "Maria",
+        "Ana"
+    )
 
     println(nomes)
 
-    for(nome in nomes){
+    for (nome in nomes) {
         println(nome)
     }
+
     println("Tem o nome alex? ${nomes.contains("Alex")}")
     println("Tamanho da colecao ${nomes.size}")
-
 }
