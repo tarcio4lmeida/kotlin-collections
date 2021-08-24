0034f1ebb26086e04e57cc385ef5072f86c8ff96
tarcio almeida <tarcioalmeidalima@gmail.com>
2021-08-24 11:36:48 -0300

    leitura e filtros especificos de map

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,37 +1,43 @@
 fun main() {
-    val pedidos = mutableMapOf<Int,Double>(
+    val pedidos: MutableMap<Int, Double> = mutableMapOf<Int, Double>(
         Pair(1, 20.0),
-        Pair(2, 34.0),
-        3 to 50.0
+        Pair(2, 64.0),
+        3 to 50.0,
+        4 to 100.0,
+        5 to 150.0
+
     )
 
-    println(pedidos)
-    val pedido = pedidos[1]
-    pedido?.let {
-        println("pedido $it")
+    val valorpedido = pedidos.getValue(4)
+    println(valorpedido)
+    println(pedidos.getOrElse(0) {
+        "Nao tem o pedido"
+    })
+
+    println(pedidos.getOrDefault(1, -1.0))
+    println(pedidos.getOrDefault(0, -1.0))
+
+    println(pedidos.keys)
+    println(pedidos.values)
+
+    for(numero in pedidos.keys){
+        println("pedidos $numero")
     }
 
-    for (p in pedidos){
-        println("pedido ${p.key} - valor ${p.value}")
+  val pedidoFilter =   pedidos.filter {
+        (numero, valor) ->
+        numero % 2 == 0 && valor > 50.0
     }
 
-    pedidos[4] = 70.0
-    println(pedidos)
+    println(pedidoFilter)
 
-    pedidos.put(5, 80.0)
+    val pedidosAcima = pedidos.filterValues { valor ->
+        valor > 70.0
+    }
 
-    pedidos.put(1, 200.0)
-    println(pedidos)
-
-    pedidos.putIfAbsent(1, 200.0)
-    println(pedidos)
-
-    pedidos.remove(1)
-    println(pedidos)
-
-    pedidos.remove(3, 70.0)
-    println(pedidos)
+    println(pedidosAcima)
 
 }
+
 
 
---
+++ src/main/kotlin/testaMap.kt
@@ -0,0 +1,34 @@
+fun testaMap() {
+    val pedidos = mutableMapOf<Int, Double>(
+        Pair(1, 20.0),
+        Pair(2, 34.0),
+        3 to 50.0
+    )
+
+    println(pedidos)
+    val pedido = pedidos[1]
+    pedido?.let {
+        println("pedido $it")
+    }
+
+    for (p in pedidos) {
+        println("pedido ${p.key} - valor ${p.value}")
+    }
+
+    pedidos[4] = 70.0
+    println(pedidos)
+
+    pedidos.put(5, 80.0)
+
+    pedidos.put(1, 200.0)
+    println(pedidos)
+
+    pedidos.putIfAbsent(1, 200.0)
+    println(pedidos)
+
+    pedidos.remove(1)
+    println(pedidos)
+
+    pedidos.remove(3, 70.0)
+    println(pedidos)
+}
